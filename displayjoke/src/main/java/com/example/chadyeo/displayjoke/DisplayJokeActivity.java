35daaf38e2f4d04b79ded69a1726123c9e7596dc
Chad Yeo <chad.yeo.cy@gmail.com>
2018-05-03 01:14:27 -0400

    Created DisplayJoke Android Library

---
+++ displayjoke/src/main/java/com/example/chadyeo/displayjoke/DisplayJokeActivity.java
@@ -0,0 +1,20 @@
+package com.example.chadyeo.displayjoke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayJokeActivity extends AppCompatActivity {
+
+    private TextView jokeTextView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_joke);
+
+        jokeTextView = (TextView) findViewById(R.id.joke_textView);
+        String jokeResult = getIntent().getStringExtra("jokeResult");
+        jokeTextView.setText(jokeResult);
+    }
+}
