cd7c7679c0d50a574dae78ea7761780ef638b4dd
Chad Yeo <chad.yeo.cy@gmail.com>
2018-05-06 15:02:17 -0400

    Fixed Asynctask Test

--- app/src/androidTest/java/AndroidJokeEndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/AndroidJokeEndpointsAsyncTaskTest.java
@@ -1,18 +1,12 @@
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.udacity.gradle.builditbigger.JokeEndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.MainActivity;
 import com.udacity.gradle.builditbigger.R;
-
-import junit.framework.Assert;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.TimeUnit;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
